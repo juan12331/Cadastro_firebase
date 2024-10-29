e212501b5ebaa16b210949a574e84761c92f9eb5
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-10-29 14:32:38 -0300

    Update tela.dart

---
+++ lib/pages/tela.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class tela extends StatelessWidget {
+  const tela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
